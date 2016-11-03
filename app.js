05b9f5548da3bd8ac0c00cb6e348479f6f92d849
eit <cheneit@gmail.com>
2016-11-03 23:18:49 +0800

    update app.js

--- app.js
+++ app.js
@@ -13,40 +13,43 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.post('/cut', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
   var result = nodejieba.cut(req.body.data);
-
-  console.log(result);
-  //res.writeHead(200, {"Content-Type": "application/json"});
+  //console.log(result);
   res.json(result);
 });
 
-app.post('/cutTrue', queryParser, function (req, res) {
-  var result = nodejieba.cut(req.body, true);
+app.post('/cutTrue', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.cut(req.body.data, true);
   res.json(result);
 });
 
-app.post('/cutHMM', queryParser, function (req, res) {
-  var result = nodejieba.cutHMM(req.body);
+app.post('/cutHMM', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.cutHMM(req.body.data);
   res.json(result);
 });
 
-app.post('/cutAll', queryParser, function (req, res) {
-  var result = nodejieba.cutAll(req.body);
+app.post('/cutAll', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.cutAll(req.body.data);
   res.json(result);
 });
 
-app.post('/cutForSearch', queryParser, function (req, res) {
-  var result = nodejieba.cutForSearch(req.body);
+app.post('/cutForSearch', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.cutForSearch(req.body.data);
   res.json(result);
 });
 
-app.post('/tag', queryParser, function (req, res) {
-  var result = nodejieba.tag(req.body);
+app.post('/tag', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.tag(req.body.data);
   res.json(result);
 });
 
-app.post('/extract', queryParser, function (req, res) {
-  var topN = 5;
-  var result = nodejieba.extract(req.body, topN);
+app.post('/extract', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400);
+  var result = nodejieba.extract(req.body.data, req.body.topk);
   res.json(result);
 });
 
